7323c2fbedca2fb707b9e86fbc85d11b2d6c602a
David Ortega Mazzini <israeldavid@gmail.com>
2023-07-25 14:00:08 -0500

    Update index.js
    Se actualizaron metodos

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hola, mundo!');
+  res.send('Bienvenido a la API de los lentes.');
 });
 
 app.get('/listarObjetivos', (req, res) => {
@@ -14,11 +14,17 @@ app.get('/listarObjetivos', (req, res) => {
   const objetivos = [
     { id: 1, descripcion: 'Sony 55mm F1.8', numeromm:55 },
     { id: 2, descripcion: 'Tamron 35mm F2.8', numeromm:35 },
-    { id: 3, descripcion: 'Sony 30mm F3.5', numeromm30:0 }
+    { id: 3, descripcion: 'Sony 30mm F3.5', numeromm30:30 }
   ];
-  res.json(tareas);
+  res.json(objetivos);
 });
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en el puerto ${port}`);
 });
+
+app.get('/listarObjetivo/:id',(req,res) => {
+  var objId = req.params.id;
+  res.send(`Bienvenido a la API de los lentes. El ID del lente es: ${objId}`);
+
+})
