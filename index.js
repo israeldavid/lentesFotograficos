8c74efc38138f8bdb907792d177a9703833a0c66
David Ortega Mazzini <israeldavid@gmail.com>
2023-07-25 13:33:44 -0500

    Cambio para el deploiy
    Se cambio el contenido de index.js y el nombre

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ app.get('/', (req, res) => {
 app.get('/listarObjetivos', (req, res) => {
   // Aquí puedes agregar la lógica para obtener la lista de tareas
   const objetivos = [
-    { id: 1, descripcion: 'Sony 55mm F1.8', estado:1 },
-    { id: 2, descripcion: 'Tamron 35mm F2.8', estado:1 },
-    { id: 3, descripcion: 'sony 30mm F3.5', estado:0 }
+    { id: 1, descripcion: 'Sony 55mm F1.8', numeromm:55 },
+    { id: 2, descripcion: 'Tamron 35mm F2.8', numeromm:35 },
+    { id: 3, descripcion: 'Sony 30mm F3.5', numeromm30:0 }
   ];
   res.json(tareas);
 });
